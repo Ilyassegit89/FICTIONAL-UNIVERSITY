9c27bcb18283148a615af0ba5f8637aed7901dc9
ILYAS <ilyasseparkour@gmail.com>
2025-07-25 17:52:22 +0100

    added WorCount setting field

--- wp-content/plugins/our-first-unique-plugin/our-first-unique-plugin.php
+++ wp-content/plugins/our-first-unique-plugin/our-first-unique-plugin.php
@@ -14,13 +14,27 @@ class WordCountAndTimePlugin{
     }
     function settings(){
         add_settings_section('wcp_first_section', null, null ,'word-count-settings-page');
+
         add_settings_field('wcp_location', 'Display Location', array($this, 'locationHTML'), 'word-count-settings-page', 'wcp_first_section');
         register_setting('wordcountplugin', 'wcp_location', array('sanitize_callback' => 'sanitize_text_field', 'default' => '0'));//register in the DB
+
+        add_settings_field('wcp_headline', 'Headline Text', array($this, 'headlineHTML'), 'word-count-settings-page', 'wcp_first_section');
+        register_setting('wordcountplugin', 'wcp_headline', array('sanitize_callback' => 'sanitize_text_field', 'default' => 'Post Statistics'));//register in the DB
+
+        add_settings_field('wcp_wordcount', 'Word Count', array($this, 'wordCountHTML'), 'word-count-settings-page', 'wcp_first_section');
+        register_setting('wordcountplugin', 'wcp_wordcount', array('sanitize_callback' => 'sanitize_text_field', 'default' => '1'));//register in the DB
     }
+    function wordCountHTML(){ ?>
+        <input type="checkbox" name="wcp_wordcount" value="1" <?php checked(get_option('wcp_wordcount'), '1')?>>
+    <?php }
+
+    function headlineHTML(){ ?>
+        <input type="text" name="wcp_headline" value="<?php echo esc_attr(get_option('wcp_headline')); ?>">
+    <?php }
     function locationHTML(){ ?>
         <select name="wcp_location">
-            <option value="0">Beginning of Post</option>
-            <option value="1">End of Post</option>
+            <option value="0" <?php selected(get_option('wcp_location', '0'))?>>Beginning of Post</option>
+            <option value="1" <?php selected(get_option('wcp_location', '1'))?>>End of Post</option>
         </select>
     <?php }
 
