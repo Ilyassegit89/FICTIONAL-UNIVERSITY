8ef4ea30c76cd6d7210b1fcf9e72ddaa8bafa4e4
Ilyassegit89 <ilyasseparkour@gmail.com>
2025-10-05 19:02:01 +0100

    added table wp_pets to db

--- wp-content/plugins/new-database-table/inc/template-pets.php
+++ wp-content/plugins/new-database-table/inc/template-pets.php
@@ -1,4 +1,6 @@
 <?php
+require_once plugin_dir_path(__FILE__) . 'GetPets.php';
+$getPets = new GetPets();
 
 get_header(); ?>
 
@@ -14,13 +16,8 @@ get_header(); ?>
 
 <div class="container container--narrow page-section">
 
-  <p>This page took <strong><?php echo timer_stop();?></strong> seconds to prepare. Found <strong>x</strong> results (showing the first x).</p>
-  <?php 
-  global $wpdb;
-  $ourQuery = $wpdb->prepare("SELECT * from wp_pets WHERE species = %s AND birthyear > %d LIMIT 50",array('hamster', 2005));
-  $pets = $wpdb->get_results($ourQuery);
-  var_dump($pets);
-  ?>
+  <p>This page took <strong><?php echo timer_stop();?></strong> seconds to prepare. Found <strong><?php echo $getPets->count?></strong> results (showing the first <?php echo count($getPets->pets)?>).</p>
+  
   <table class="pet-adoption-table">
     <tr>
       <th>Name</th>
@@ -30,9 +27,13 @@ get_header(); ?>
       <th>Hobby</th>
       <th>Favorite Color</th>
       <th>Favorite Food</th>
+      <?php 
+      if(current_user_can('administrator')){ ?>
+                  <th>Delete</th>
+      <?php } ?>
     </tr>
     <?php
-    foreach($pets as $pet){?> 
+    foreach($getPets->pets as $pet){?> 
     <tr>
       <td><?php echo $pet->petname; ?></td>
       <td><?php echo $pet->species; ?></td>
@@ -41,10 +42,32 @@ get_header(); ?>
       <td><?php echo $pet->favhobby; ?></td>
       <td><?php echo $pet->favcolor; ?></td>
       <td><?php echo $pet->favfood; ?></td>
+      <?php 
+      if(current_user_can('administrator')){ ?>
+        <td style="text-align: center">
+            <form action="<?php echo esc_url(admin_url('admin-post.php'))?>" method="POST">
+              <input type="hidden" name="action" value="deletepet"/>
+              <input type="hidden" name="idtodelete" value="<?php echo $pet->id; ?>"/>
+              <button class="delete-pet-button">X</button>
+            </form>
+        </td>
+      <?php }
+      ?>
+      
     </tr>
     <?php }
     ?>
   </table>
+  <?php
+    if(current_user_can("administrator")){ ?>
+      <form action="<?php echo esc_url(admin_url('admin-post.php'))?>" class="create-pet-form" method="POST">
+          <p>Enter Just the name for a new pet. Its species, weight, and other details will be generated</p>
+          <input type="hidden" name="action" value="createpet"/>
+          <input type="text" name="incomingpetname" placeholder="name..."/>
+          <button>Add Pet</button>
+      </form>
+    <?php }
+  ?>
   
 </div>
 
--- wp-content/plugins/new-database-table/new-database-table.php
+++ wp-content/plugins/new-database-table/new-database-table.php
@@ -18,8 +18,37 @@ class PetAdoptionTablePlugin {
 
     add_action('activate_new-database-table/new-database-table.php', array($this, 'onActivate'));
     //add_action('admin_head', array($this, 'populateFast'));
+    add_action('admin_post_createpet', array($this, 'createPet'));
+    add_action('admin_post_nopriv_createpet', array($this, 'createPet'));
+     add_action('admin_post_deletepet', array($this, 'deletePet'));
+    add_action('admin_post_nopriv_deletepet', array($this, 'deletePet'));
     add_action('wp_enqueue_scripts', array($this, 'loadAssets'));
     add_filter('template_include', array($this, 'loadTemplate'), 99);
+  }
+
+  function deletePet(){
+    if(current_user_can('administrator')){
+      $id = sanitize_text_field($_POST['idtodelete']);
+      global $wpdb;
+      $wpdb->delete($this->tablename, array('id' => $id));
+      wp_safe_redirect(site_url('/pet-adoption'));
+    }else{
+      wp_safe_redirect(site_url());
+    }
+    exit;
+  }
+
+  function createPet(){
+    if(current_user_can('administrator')){
+      $pet = generatePet();
+      $pet['petname'] = sanitize_text_field($_POST['incomingpetname']);
+      global $wpdb;
+      $wpdb->insert($this->tablename, $pet);
+      wp_safe_redirect(site_url('/pet-adoption'));
+    }else{
+      wp_safe_redirect(site_url());
+    }
+    exit;
   }
 
   function onActivate() {
@@ -57,7 +86,7 @@ class PetAdoptionTablePlugin {
 
   function populateFast() {
     $query = "INSERT INTO $this->tablename (`species`, `birthyear`, `petweight`, `favfood`, `favhobby`, `favcolor`, `petname`) VALUES ";
-    $numberofpets = 20;
+    $numberofpets = 98;
     for ($i = 0; $i < $numberofpets; $i++) {
       $pet = generatePet();
       $query .= "('{$pet['species']}', {$pet['birthyear']}, {$pet['petweight']}, '{$pet['favfood']}', '{$pet['favhobby']}', '{$pet['favcolor']}', '{$pet['petname']}')";
