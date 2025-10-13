ba6c47fed317ba79bc0b384234775853ecb7f71d
Ilyassegit89 <ilyasseparkour@gmail.com>
2025-10-13 21:21:45 +0100

    Added the Professor Callout and save it to DB

--- wp-content/plugins/featured-professor/src/index.js
+++ wp-content/plugins/featured-professor/src/index.js
@@ -1,24 +1,41 @@
+import { useEffect } from "react"
 import "./index.scss"
+import {useSelect} from "@wordpress/data"
 
 wp.blocks.registerBlockType("ourplugin/featured-professor", {
   title: "Professor Callout",
   description: "Include a short description and link to a professor of your choice",
   icon: "welcome-learn-more",
   category: "common",
+  attributes: {
+    profId: {type: "string"}
+  },
   edit: EditComponent,
   save: function () {
     return null
   }
 })
 
-function EditComponent() {
+function EditComponent(props) {
+  const allProfs = useSelect(select => {
+    return select("core").getEntityRecords("postType", "professor", {per_page: -1})
+  })
+  console.log(allProfs)
+
+  if(allProfs == undefined) return <p>Loading...</p>
+
   return (
     <div className="featured-professor-wrapper">
       <div className="professor-select-container">
-        <select>
-          <option value="1">1</option>
-          <option value="2">2</option>
-          <option value="3">3</option>
+        <select onChange={e => props.setAttributes({profId: e.target.value})}>
+          <option value="">Select a professor</option>
+          {allProfs.map(prof => {
+            return(
+              <option value={prof.id} selected={props.attributes.profId == prof.id}>{prof.title.rendered}</option>
+            )
+          })}
+          
+
         </select>
       </div>
       <div>
